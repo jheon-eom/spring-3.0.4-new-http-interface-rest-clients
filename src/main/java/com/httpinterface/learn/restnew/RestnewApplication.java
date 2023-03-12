de64e8fad67ab74ca0ad7d5c4959b54f54468045
jheon-eom <e4033jh@daum.net>
2023-03-13 08:53:14 +0900

    프로젝트 세팅

---
+++ src/main/java/com/httpinterface/learn/restnew/RestnewApplication.java
@@ -0,0 +1,13 @@
+package com.httpinterface.learn.restnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/httpinterface/learn/restnew/RestnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httpinterface.learn.restnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
